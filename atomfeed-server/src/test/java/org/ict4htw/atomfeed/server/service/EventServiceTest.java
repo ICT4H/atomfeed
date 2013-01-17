e2e991da40c9e17c3b76b7ef126567b33efc63a4
Chris Ford <christophertford@gmail.com>
2013-01-17 12:19:08 +0530

    Made test more literal.

--- atomfeed-server/src/test/java/org/ict4htw/atomfeed/server/service/EventServiceTest.java
+++ atomfeed-server/src/test/java/org/ict4htw/atomfeed/server/service/EventServiceTest.java
@@ -4,26 +4,25 @@ import org.ict4htw.atomfeed.server.domain.EventRecord;
 import org.ict4htw.atomfeed.server.repository.AllEventRecordsStub;
 import org.joda.time.DateTime;
 import org.junit.Test;
-
 import java.net.URISyntaxException;
 import java.util.Arrays;
-import java.util.UUID;
+import java.util.Collections;
+import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
 
 public class EventServiceTest {
     @Test
     public void shouldNotifyEventServiceOfEventAndStoreIt() throws URISyntaxException {
         AllEventRecordsStub allEventRecords = new AllEventRecordsStub();
         EventService eventService = new EventService(allEventRecords);
-        String uuid = UUID.randomUUID().toString();
-        Event event = new Event(uuid, "title", DateTime.now(), "http://uri", Arrays.asList("asd", "dsa", "zzz"));
+        String uuid = "51850820-6071-11e2-bcfd-0800200c9a66";
+        List<String> serializableObject = Collections.emptyList();
 
+        Event event = new Event(uuid, "title", new DateTime(123), "http://uri", serializableObject);
         eventService.notify(event);
-
         EventRecord eventRecord = allEventRecords.get(uuid);
-        assertNotNull(eventRecord);
+
         assertEquals(eventRecord.getTitle(), event.getTitle());
     }
 }
