19626422674fc4e8c55ebe9d4e8f96b8971a175c
Chris Ford <christophertford@gmail.com>
2013-01-23 20:20:49 +0530

    Use an iterator to make selecting the entries we're interested in easier.

--- atomfeed-client/src/main/java/org/ict4htw/atomfeed/client/FeedEnumerator.java
+++ atomfeed-client/src/main/java/org/ict4htw/atomfeed/client/FeedEnumerator.java
@@ -1,19 +1,19 @@
 package org.ict4htw.atomfeed.client;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.log4j.Logger;
-import org.ict4htw.atomfeed.client.domain.Entries;
-import org.ict4htw.atomfeed.client.repository.AllFeeds;
-
 import com.sun.syndication.feed.atom.Entry;
 import com.sun.syndication.feed.atom.Feed;
 import com.sun.syndication.feed.atom.Link;
+import org.apache.log4j.Logger;
+import org.ict4htw.atomfeed.client.repository.AllFeeds;
 
-public class FeedEnumerator {
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class FeedEnumerator implements Iterable<Entry> {
     private AllFeeds allFeeds;
     private URI startingURI;
     private static Logger logger = Logger.getLogger(FeedEnumerator.class);
@@ -23,70 +23,27 @@ public class FeedEnumerator {
         this.startingURI = startingURI;
     }
 
-    private Feed navigateToFeedEntry(String feedEntryId) throws URISyntaxException {
-        return feedWith(startingURI, feedEntryId);
-    }
+    public List<Entry> newerEntries(String lastReadEntryId) throws URISyntaxException {
+        List<Entry> entries = new ArrayList<Entry>();
+        for (Entry entry : this) {
+            if(entry.getId().equals(lastReadEntryId)) {
+                Collections.reverse(entries);
+                return entries;
+            }
+            entries.add(entry);
+        }
+		throw new RuntimeException("Entry not found.");
 
-    private Feed feedWith(URI uri, String feedEntryId) throws URISyntaxException {
-        Feed feed = allFeeds.getFor(uri);
-        Entries feedEntries = new Entries(feed.getEntries());
-        if (feedEntries.getEntryWith(feedEntryId) != null)
-            return feed;
-        URI prevArchiveURI = getPrevArchiveURI(feed);
-		if (prevArchiveURI != null)
-			return feedWith(prevArchiveURI, feedEntryId);
-		else
-			throw new RuntimeException("Entry does not exist in any feed.");
-    }    
-
-	public List<Entry> newerEntries(String lastReadEntryId) throws URISyntaxException {
-        List<Entry> entryList = new ArrayList<Entry>();
-        Feed feed = navigateToFeedEntry(lastReadEntryId);
-        if (feed == null) return entryList;
-
-        // Add all the entries in that feedgenerator which are newer
-        Entries entries = new Entries(feed.getEntries());
-        entryList.addAll(entries.newerEntries(lastReadEntryId));
-
-        // Recurse through all the newer feeds and add all entries
-        addNewerEntries(feed, entryList);
-        return entryList;
     }
 	
 	public List<Entry> getAllEntries() throws URISyntaxException {
-		URI feedURI = startingURI;
-		List<Entry> entries = new ArrayList<Entry>();
-		do {
-			Feed feed = allFeeds.getFor(feedURI);
-			List<Entry> updatedList = feed.getEntries();
-			updatedList.addAll(entries);
-			entries = updatedList;
-			feedURI = getPrevArchiveURI(feed);
-		} while (feedURI != null);
+        List<Entry> entries = new ArrayList<Entry>();
+        for (Entry feed : this) {
+            entries.add(feed);
+        }
+        Collections.reverse(entries);
 		return entries;
 	}
-	
-	
-	
-
-    private void addNewerEntries(Feed feed, List<Entry> entryList) throws URISyntaxException {
-        URI uri = getNextArchive(feed);
-        Feed currentFeed = allFeeds.getFor(uri);
-        if (currentFeed != null) {
-            entryList.addAll(currentFeed.getEntries());
-            addNewerEntries(currentFeed, entryList);
-        }
-    }
-
-    private URI getPrevArchiveURI(Feed feed) throws URISyntaxException {
-        URI uriFromNamedLink = getUriFromNamedLink("prev-archive", feed);
-        logger.info("prev-link URI: " + uriFromNamedLink);
-        return uriFromNamedLink;
-    }
-
-    private URI getNextArchive(Feed feed) throws URISyntaxException {
-        return getUriFromNamedLink("next-archive", feed);
-    }
 
     private URI getUriFromNamedLink(String relValue, Feed feed) throws URISyntaxException {
         for (Object obj : feed.getOtherLinks()) {
@@ -96,5 +53,52 @@ public class FeedEnumerator {
             }
         }
         return null;
-    }	
+    }
+
+    @Override
+    public Iterator<Entry> iterator() {
+        return new Iterator<Entry>() {
+
+            Feed feed = allFeeds.getFor(startingURI);
+            List<Entry> entries;
+
+            {
+                setEntries();
+            }
+
+            private void setEntries() {
+                entries = feed.getEntries();
+                Collections.reverse(entries);
+            }
+
+            private URI prev() {
+                try {
+                    return getUriFromNamedLink("prev-archive", feed);
+                } catch (URISyntaxException e) {
+                    throw new RuntimeException("Bad prev link.");
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                return !entries.isEmpty();
+            }
+
+            @Override
+            public Entry next() {
+                Entry entry = entries.get(0);
+                remove();
+                return entry;
+            }
+
+            @Override
+            public void remove() {
+               entries.remove(0);
+               if (prev() != null && entries.isEmpty()) {
+                   feed = allFeeds.getFor(prev());
+                   setEntries();
+               }
+            };
+        };
+    }
 }
