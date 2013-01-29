a993716ee3f368fbdd742da767b54bae47287522
Chris Ford <christophertford@gmail.com>
2013-01-29 18:07:31 +0530

    Removed boilerplate comments.

--- atomfeed-server/src/main/java/org/ict4htw/atomfeed/server/resource/AtomResponse.java
+++ atomfeed-server/src/main/java/org/ict4htw/atomfeed/server/resource/AtomResponse.java
@@ -1,12 +1,5 @@
 package org.ict4htw.atomfeed.server.resource;
 
-/**
- * Created with IntelliJ IDEA.
- * User: karthik
- * Date: 07/01/13
- * Time: 3:36 PM
- * To change this template use File | Settings | File Templates.
- */
 public class AtomResponse {
     private ResponseStatus status;
 
