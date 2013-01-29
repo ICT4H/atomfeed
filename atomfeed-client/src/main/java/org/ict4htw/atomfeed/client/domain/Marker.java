3bcecea8a310073058449d177511f118415570cf
Karthik <kartheet@thoughtworks.com>
2013-01-29 12:28:14 +0530

    added tests for AllMarkers

--- atomfeed-client/src/main/java/org/ict4htw/atomfeed/client/domain/Marker.java
+++ atomfeed-client/src/main/java/org/ict4htw/atomfeed/client/domain/Marker.java
@@ -18,4 +18,24 @@ public class Marker {
 	public URI getFeedUri() {
 		return feedUri;
 	}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Marker marker = (Marker) o;
+
+        if (entryId != null ? !entryId.equals(marker.entryId) : marker.entryId != null) return false;
+        if (feedUri != null ? !feedUri.equals(marker.feedUri) : marker.feedUri != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = entryId != null ? entryId.hashCode() : 0;
+        result = 31 * result + (feedUri != null ? feedUri.hashCode() : 0);
+        return result;
+    }
 }
