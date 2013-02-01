c8601d8420ef7587dd8fd0771ca72e681e278d10
Chris Ford <christophertford@gmail.com>
2013-02-01 11:57:26 +0530

    Cleaned up event record creator.

--- atomfeed-server/src/test/java/org/ict4htw/atomfeed/server/repository/InMemoryEventRecordCreator.java
+++ atomfeed-server/src/test/java/org/ict4htw/atomfeed/server/repository/InMemoryEventRecordCreator.java
@@ -1,10 +1,10 @@
 package org.ict4htw.atomfeed.server.repository;
 
+import org.ict4htw.atomfeed.server.domain.EventRecord;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Calendar;
-
-import org.ict4htw.atomfeed.server.domain.EventRecord;
 
 public class InMemoryEventRecordCreator {
     private AllEventRecords allEventRecords;
@@ -13,19 +13,20 @@ public class InMemoryEventRecordCreator {
         this.allEventRecords = allEventRecords;
     }
 
-    public int create(int numOfEvents) throws URISyntaxException {
+    public void create(int numOfEvents) throws URISyntaxException {
         addEvents(numOfEvents);
-        return allEventRecords.getTotalCount();
     }
     
     private void addEvents(int numOfEvents) throws URISyntaxException {
     	Calendar cal = Calendar.getInstance();
-        for (int idx= 1; idx <= numOfEvents; idx++) {
+        for (int index= 1; index <= numOfEvents; index++) {
         	cal.add(Calendar.SECOND, 1);
             allEventRecords.add(new EventRecord(
-            		"uuid" + idx, "title" + idx, 
-            		new URI("http://foo.bar/"+idx), 
-            		"Event" + idx, cal.getTime()) );
+            		"uuid" + index,
+                    "title" + index,
+            		new URI("http://foo.bar/" + index),
+            		"Event " + index + "'s contents.",
+                    cal.getTime()));
         }
     }
 }
