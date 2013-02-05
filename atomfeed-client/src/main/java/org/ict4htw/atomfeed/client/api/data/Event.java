f565b22b87b789c99701c4cc5add82dd2b765b73
Karthik <kartheet@thoughtworks.com>
2013-02-05 12:53:48 +0530

    added get payload to client event object

--- atomfeed-client/src/main/java/org/ict4htw/atomfeed/client/api/data/Event.java
+++ atomfeed-client/src/main/java/org/ict4htw/atomfeed/client/api/data/Event.java
@@ -1,15 +1,34 @@
 package org.ict4htw.atomfeed.client.api.data;
 
+import com.sun.syndication.feed.atom.Content;
 import com.sun.syndication.feed.atom.Entry;
 
 public class Event {
+
     private Entry entry;
+
 
 	public Event(Entry entry) {
 		this.entry = entry;
 	}
 
+    /**
+     * returns the id of Entry
+     * @return
+     */
     public String getId() {
         return entry.getId();
+    }
+
+    /**
+     *
+     * reads the list of contents of entry and returns the first content's value as string
+     * for now as a contract we have only one content in the list.
+     *
+     * @return
+     */
+    public String getContent(){
+        Content content = (Content) entry.getContents().get(0);
+        return entry.getContents().size()!=0? content.getValue() :null;
     }
 }
