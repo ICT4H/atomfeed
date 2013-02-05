dd8046b062782a6e33d3742c0027c59336da3c98
vi <flawed.flint@gmail.com>
2013-02-05 18:26:00 +0530

    client Integration test - IInd cut

--- atomfeed-client/src/test/java/org/ict4htw/atomfeed/client/AtomFeedClientIT.java
+++ atomfeed-client/src/test/java/org/ict4htw/atomfeed/client/AtomFeedClientIT.java
@@ -1,26 +1,33 @@
 package org.ict4htw.atomfeed.client;
 
-import junit.framework.Assert;
+import com.sun.syndication.feed.atom.Feed;
 import org.ict4htw.atomfeed.SpringIntegrationIT;
-import org.ict4htw.atomfeed.server.domain.EventRecord;
+import org.ict4htw.atomfeed.client.repository.AllFeeds;
+import org.ict4htw.atomfeed.client.repository.datasource.WebClient;
 import org.ict4htw.atomfeed.server.repository.DbEventRecordCreator;
 import org.ict4htw.atomfeed.server.repository.jdbc.AllEventRecordsJdbcImpl;
+import com.sun.syndication.feed.atom.Entry;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 import java.util.UUID;
+
+import static junit.framework.Assert.assertEquals;
 
 public class AtomFeedClientIT extends SpringIntegrationIT{
 
     private DbEventRecordCreator recordCreator;
     private Connection connection;
     private AllEventRecordsJdbcImpl eventRecords;
+    private AllFeeds allFeeds;
 
     @Before
     public void before() throws SQLException {
@@ -30,6 +37,7 @@ public class AtomFeedClientIT extends SpringIntegrationIT{
         statement.close();
         eventRecords = new AllEventRecordsJdbcImpl(getProvider(connection));
         recordCreator = new DbEventRecordCreator(eventRecords);
+        allFeeds = new AllFeeds(new WebClient());
     }
 
     @After
@@ -37,12 +45,22 @@ public class AtomFeedClientIT extends SpringIntegrationIT{
         connection.close();
     }
 
-    @Test
+    //TODO - Set Chunking history chunk_size so that we always query for the first feed. Simplicity.
+    //Spawn mvn jetty:run - either from here or a script outside.
+    // Also, this should be run on Travis.
+    //Set atom mime type on response from server, so that response can be de-Serialized appropriately.
+    // Make the spawned mvn jetty server read from the DB all the while to ensure that this test passes.
+    @Ignore
     public void shouldReadEventsCreatedEvents() throws URISyntaxException {
         String uuid = UUID.randomUUID().toString();
         createOneEvent(uuid,"One Event","http://google.com");
-        EventRecord eventRecord = eventRecords.get(uuid);
-        Assert.assertNotNull(eventRecord);
+        Integer id = eventRecords.get(uuid).getId();
+        URI uri = new URI(String.format("http://localhost:8080/events/1"));
+        Feed feed = allFeeds.getFor(uri);
+        List entries = feed.getEntries();
+        assertEquals(1, entries.size());
+        Entry entry = (Entry) entries.get(0);
+        assertEquals(entry.getId(),uuid);
     }
 
     private void createOneEvent(String uuid,String title, String url) throws URISyntaxException {
