abbe6bed02ff3c6cb653b04913052ade4d88ea02
vi <flawed.flint@gmail.com>
2013-02-13 18:42:02 +0530

    got rid of unrequired mapper. Refactored a bit of the code in the consumer.

--- motech-atomfeed-consumer/src/main/java/org/ict4htw/atomfeed/motechclient/MotechAtomFeedConsumer.java
+++ motech-atomfeed-consumer/src/main/java/org/ict4htw/atomfeed/motechclient/MotechAtomFeedConsumer.java
@@ -5,7 +5,6 @@ import org.ict4htw.atomfeed.client.api.FeedClient;
 import org.ict4htw.atomfeed.client.api.data.Event;
 import org.ict4htw.atomfeed.client.repository.AllFeeds;
 import org.ict4htw.atomfeed.client.repository.AllMarkers;
-import org.ict4htw.atomfeed.client.repository.datasource.MarkerDataSource;
 import org.ict4htw.atomfeed.client.repository.datasource.WebClient;
 import org.motechproject.event.MotechEvent;
 import org.motechproject.event.listener.EventRelay;
@@ -18,26 +17,23 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.List;
 
 @Component
 public class MotechAtomFeedConsumer {
 
+    public static final String EVENT_FROM_OPEN_MRS = "eventFromOpenMRS";
     private URI entryURL;
-    private EventMapper eventMapper;
     private MotechSchedulerService schedulerService;
-    private static String ATOM_UPDATE_MESSAGE="atomUpdateMessage";
+    private static final String ATOM_UPDATE_MESSAGE="atomUpdateMessage";
     private String cronExpression;
     private EventRelay eventRelay;
     private FeedClient feedClient;
 
     @PostConstruct
     public void startScheduler(){
-        schedulerService.scheduleJob(
-                new CronSchedulableJob(
-                        new MotechEvent(ATOM_UPDATE_MESSAGE),
-                        cronExpression
-                ));
+        schedulerService.scheduleJob(new CronSchedulableJob(new MotechEvent(ATOM_UPDATE_MESSAGE),cronExpression));
     }
 
     @PreDestroy
@@ -45,19 +41,17 @@ public class MotechAtomFeedConsumer {
         schedulerService.safeUnscheduleAllJobs(ATOM_UPDATE_MESSAGE);
     }
 
-    public MotechAtomFeedConsumer(URI startingURL, WebClient webClient, EventMapper eventMapper,
+    public MotechAtomFeedConsumer(URI startingURL, WebClient webClient,
                                   String cronExpression, EventRelay eventRelay, MotechSchedulerService schedulerService) throws URISyntaxException {
         this.cronExpression = cronExpression;
         this.entryURL = startingURL;
         this.eventRelay = eventRelay;
         this.schedulerService = schedulerService;
-        this.eventMapper = eventMapper;
         AllFeeds allFeeds = new AllFeeds(webClient);
-        MarkerDataSource inmemoryMarkerDataSource = new InmemoryMarkerDataSource();
-        feedClient=new AtomFeedClient(allFeeds, new AllMarkers(inmemoryMarkerDataSource));
+        feedClient = new AtomFeedClient(allFeeds, new AllMarkers(new InmemoryMarkerDataSource()));
     }
 
-    @MotechListener(subjects = "atomUpdateMessage")
+    @MotechListener(subjects = ATOM_UPDATE_MESSAGE)
     public void updateEvents(MotechEvent event) throws URISyntaxException {
         Thread thread = Thread.currentThread();
         ClassLoader oldContextClassLoader = thread.getContextClassLoader();
@@ -75,7 +69,9 @@ public class MotechAtomFeedConsumer {
     private void update() throws URISyntaxException {
         List<Event> events = feedClient.unprocessedEvents(entryURL);
         for (Event event : events) {
-            eventRelay.sendEventMessage(eventMapper.map(event));
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("contents", event.getContent());
+            eventRelay.sendEventMessage(new MotechEvent(EVENT_FROM_OPEN_MRS,map));
         }
         Event lastEvent = events.get(events.size() - 1);
         feedClient.processedTo(entryURL,lastEvent.getId());
