60e8a5e6c99944e587ce07be1a1daf5da6a30393
vi <flawed.flint@gmail.com>
2013-02-21 12:57:53 +0530

    removing unused motech atomfeed servermodule. Can always fetch this from git if needed

--- motech-atomfeed-server/src/main/java/org/ict4h/atomfeed/motechserver/MotechEventToEventMapper.java
+++
@@ -1,8 +0,0 @@
-package org.ict4h.atomfeed.motechserver;
-
-import org.ict4htw.atomfeed.server.service.Event;
-import org.motechproject.event.MotechEvent;
-
-public interface MotechEventToEventMapper {
-    Event map(MotechEvent event);
-}
