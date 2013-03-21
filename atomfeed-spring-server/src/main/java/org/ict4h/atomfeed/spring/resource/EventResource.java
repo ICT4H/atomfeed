503f39508382082798341bc5c159ac5a25a2c36b
Chris Ford <christophertford@gmail.com>
2013-03-21 11:32:14 +0300

    Removed notify URL because it has no security.

--- atomfeed-spring-server/src/main/java/org/ict4h/atomfeed/spring/resource/EventResource.java
+++ atomfeed-spring-server/src/main/java/org/ict4h/atomfeed/spring/resource/EventResource.java
@@ -4,12 +4,14 @@ import com.sun.syndication.feed.atom.Feed;
 import com.sun.syndication.io.FeedException;
 import com.sun.syndication.io.WireFeedOutput;
 import org.apache.log4j.Logger;
-import org.ict4h.atomfeed.server.service.Event;
 import org.ict4h.atomfeed.server.service.EventFeedService;
 import org.ict4h.atomfeed.server.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
@@ -56,13 +58,5 @@ public class EventResource {
             throw new RuntimeException("Error serializing feed.", e);
         }
         //TODO: check comments in getRecentFeed()
-    }
-
-
-    @RequestMapping(method = RequestMethod.POST, value = "/events")
-    public AtomResponse notifyEvent(@ModelAttribute Event event){
-        //TODO Security !!
-        eventService.notify(event);
-        return new AtomResponse(AtomResponse.ResponseStatus.SUCCESS);
     }
 }
