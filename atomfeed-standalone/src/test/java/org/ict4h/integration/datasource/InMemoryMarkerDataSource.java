2a01112bfb283503b941c43e84787b0e81b3a592
vi <flawed.flint@gmail.com>
2013-03-22 14:24:14 +0530

    integration test for end to end feed integration using atom feed client.

--- atomfeed-standalone/src/test/java/org/ict4h/integration/datasource/InMemoryMarkerDataSource.java
+++ atomfeed-standalone/src/test/java/org/ict4h/integration/datasource/InMemoryMarkerDataSource.java
@@ -1,15 +1,16 @@
-package org.ict4h.atomfeed.client.repository.datasource;
+package org.ict4h.integration.datasource;
 
 import org.ict4h.atomfeed.client.domain.Marker;
+import org.ict4h.atomfeed.client.repository.datasource.MarkerDataSource;
 
 import java.net.URI;
 import java.util.HashMap;
 
-public class MemoryMarkerDataSource implements MarkerDataSource{
+public class InMemoryMarkerDataSource implements MarkerDataSource {
 
     private final HashMap<URI,Marker> map;
 
-    public MemoryMarkerDataSource() {
+    public InMemoryMarkerDataSource() {
         map = new HashMap<URI,Marker>();
     }
 
