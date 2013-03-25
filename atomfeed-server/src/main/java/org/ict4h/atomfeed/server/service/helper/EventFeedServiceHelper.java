b7aeee66b6868a4c6fe93e18f9d57aef094c2b96
vi <flawed.flint@gmail.com>
2013-03-25 12:10:34 +0530

    moving controller actions to a helper in server so that code can be reused.

---
+++ atomfeed-server/src/main/java/org/ict4h/atomfeed/server/service/helper/EventFeedServiceHelper.java
@@ -0,0 +1,39 @@
+package org.ict4h.atomfeed.server.service.helper;
+
+import com.sun.syndication.feed.atom.Feed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.WireFeedOutput;
+import org.apache.log4j.Logger;
+import org.ict4h.atomfeed.server.service.EventFeedService;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class EventFeedServiceHelper {
+    public static String getRecentFeed(EventFeedService eventFeedService, String requestURL, Logger logger ){
+        try {
+            Feed feed = eventFeedService.getRecentFeed(new URI(requestURL));
+            return new WireFeedOutput().outputString(feed);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Bad URI", e);
+        } catch (Exception e) {
+            logger.error("error occurred while getting recent feedgenerator", e);
+            //TODO: should throw exception that should be either contextual error like bad request
+            //to be resolved by an exception resolver to  return error code 400 or so
+            throw new RuntimeException("Unexpected error", e); //TODO
+        }
+    }
+
+    public static String getEventFeed(EventFeedService eventFeedService, String requestURL, int feedNumber, Logger logger){
+        try {
+            Feed feed = eventFeedService.getEventFeed(new URI(requestURL), feedNumber);
+            return new WireFeedOutput().outputString(feed);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Bad URI", e);
+        } catch (FeedException e) {
+            logger.error("error occurred while getting recent feedgenerator", e);
+            throw new RuntimeException("Error serializing feed.", e);
+        }
+        //TODO: check comments in getRecentFeed()
+    }
+}
--- atomfeed-spring-server/src/main/java/org/ict4h/atomfeed/spring/resource/EventResource.java
+++ atomfeed-spring-server/src/main/java/org/ict4h/atomfeed/spring/resource/EventResource.java
@@ -1,10 +1,8 @@
 package org.ict4h.atomfeed.spring.resource;
 
-import com.sun.syndication.feed.atom.Feed;
-import com.sun.syndication.io.FeedException;
-import com.sun.syndication.io.WireFeedOutput;
 import org.apache.log4j.Logger;
 import org.ict4h.atomfeed.server.service.EventFeedService;
+import org.ict4h.atomfeed.server.service.helper.EventFeedServiceHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @Controller
 public class EventResource {
@@ -29,30 +25,12 @@ public class EventResource {
     @RequestMapping(method = RequestMethod.GET, value = "/feed/recent", produces = "application/atom+xml")
     @ResponseBody
     public String getRecentEventFeed(HttpServletRequest httpServletRequest) {
-    	try {
-            Feed feed = eventFeedService.getRecentFeed(new URI(httpServletRequest.getRequestURL().toString()));
-            return new WireFeedOutput().outputString(feed);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException("Bad URI", e);
-        } catch (Exception e) {
-        	logger.error("error occurred while getting recent feedgenerator", e);
-        	//TODO: should throw exception that should be either contextual error like bad request
-        	//to be resolved by an exception resolver to  return error code 400 or so
-        	throw new RuntimeException("Unexpected error", e); //TODO
-        }
+        return EventFeedServiceHelper.getRecentFeed(eventFeedService,httpServletRequest.getRequestURL().toString(),logger);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/feed/{n}")
     @ResponseBody
     public String getEventFeed(HttpServletRequest httpServletRequest, @PathVariable int n) {
-    	try {
-            Feed feed = eventFeedService.getEventFeed(new URI(httpServletRequest.getRequestURL().toString()), n);
-            return new WireFeedOutput().outputString(feed);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException("Bad URI", e);
-        } catch (FeedException e) {
-            throw new RuntimeException("Error serializing feed.", e);
-        }
-        //TODO: check comments in getRecentFeed()
+        return EventFeedServiceHelper.getEventFeed(eventFeedService,httpServletRequest.getRequestURL().toString(),n,logger);
     }
 }
