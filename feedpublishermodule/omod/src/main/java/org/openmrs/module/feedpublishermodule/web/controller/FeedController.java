fac20e5002b6ba99678f15282b6a50b8b2e135f0
vi <flawed.flint@gmail.com>
2013-03-26 16:48:39 +0530

    added support to retieve feeds given a feedId in OpenMRS module

--- feedpublishermodule/omod/src/main/java/org/openmrs/module/feedpublishermodule/web/controller/FeedController.java
+++ feedpublishermodule/omod/src/main/java/org/openmrs/module/feedpublishermodule/web/controller/FeedController.java
@@ -22,6 +22,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -41,10 +42,21 @@ public class FeedController {
 
     @RequestMapping(value = "feed/recent", method = RequestMethod.GET)
     @ResponseBody
-	public ResponseEntity<String> get(HttpServletRequest request){
+	public ResponseEntity<String> getRecentFeed(HttpServletRequest request){
         String response = EventFeedServiceHelper.getRecentFeed(eventFeedService,request.getRequestURL().toString(),logger);
+        return respondWithHeaders(response);
+	}
+
+    @RequestMapping( value = "/feed/{n}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<String> getEventFeed(HttpServletRequest httpServletRequest, @PathVariable int n) {
+        String response = EventFeedServiceHelper.getEventFeed(eventFeedService, httpServletRequest.getRequestURL().toString(), n, logger);
+        return respondWithHeaders(response);
+    }
+
+    private ResponseEntity<String> respondWithHeaders(String response) {
         MultiValueMap<String, String> headers = new HttpHeaders();
         headers.add("Content-Type", "application/atom+xml");
-        return new ResponseEntity<String>(response, headers,HttpStatus.OK);
-	}
+        return new ResponseEntity<String>(response, headers, HttpStatus.OK);
+    }
 }
