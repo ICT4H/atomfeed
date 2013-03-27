7fef4bbd695ddb768a163a433ae9ac6bc9d62aa6
vi <flawed.flint@gmail.com>
2013-03-27 19:28:29 +0530

    added javadoc for event feed service

--- atomfeed-server/src/main/java/org/ict4h/atomfeed/server/service/EventFeedService.java
+++ atomfeed-server/src/main/java/org/ict4h/atomfeed/server/service/EventFeedService.java
@@ -4,10 +4,25 @@ import java.net.URI;
 
 import com.sun.syndication.feed.atom.Feed;
 
+/**
+ * The interface {@code EventFeedService} contains methods to retrieve a {@code Feed}.
+ *
+ */
 public interface EventFeedService {
 
+    /**
+     * Returns the most recent {@code Feed}
+     * @param requestUri  a URI that identifies the recent {@code Feed}.
+     * @return The {@code Feed} identified by the requestUri
+     */
 	public abstract Feed getRecentFeed(URI requestUri);
 
+    /**
+     * Returns the {@code Feed} given a {@code URI} and {@code Integer} identifier for a feed
+     * @param requestUri a URI that is the request for the given feed
+     * @param feedId an integer that refers the the feed
+     * @return The {@code Feed} identified by the feedId parameter
+     */
 	public abstract Feed getEventFeed(URI requestUri, Integer feedId);
 
 }
