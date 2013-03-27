ac636b8a61de58b79428b0e3bd11a9801aefa6ac
vi <flawed.flint@gmail.com>
2013-03-27 19:10:07 +0530

    JavaDoc for EventService

--- atomfeed-server/src/main/java/org/ict4h/atomfeed/server/service/EventService.java
+++ atomfeed-server/src/main/java/org/ict4h/atomfeed/server/service/EventService.java
@@ -1,5 +1,11 @@
 package org.ict4h.atomfeed.server.service;
 
 public interface EventService {
+    /**
+     * Publishes an {@code Event}. This results in an {@code Event} being created
+     * in the underlying data store.
+     * @param event an {@code Event}
+     * @throws RuntimeException when an {@code Event} cannot be created.
+     */
     public void notify(Event event);
 }
