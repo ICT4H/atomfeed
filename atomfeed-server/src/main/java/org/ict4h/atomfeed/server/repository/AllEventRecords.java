b2b9d59a2312514d306d1fe41f1f014bb1f4940c
vi <flawed.flint@gmail.com>
2013-03-27 20:10:04 +0530

    JavaDoc for All Event Records

--- atomfeed-server/src/main/java/org/ict4h/atomfeed/server/repository/AllEventRecords.java
+++ atomfeed-server/src/main/java/org/ict4h/atomfeed/server/repository/AllEventRecords.java
@@ -5,15 +5,48 @@ import java.util.List;
 import org.ict4h.atomfeed.server.domain.EventRecord;
 import org.ict4h.atomfeed.server.domain.timebasedchunkingconfiguration.TimeRange;
 
+/**
+ * The interface {@code AllEventRecords} contains methods to perform {@code EventRecord} retrieval and
+ * addition.
+ */
 public interface AllEventRecords {
 
+    /**
+     * Adds an {@code EventRecord} to the underlying data store
+     * @param eventRecord an {@code EventRecord} to be created
+     * @throws RuntimeException when creation of an {@code EventRecord} is not successful
+     */
 	void add(EventRecord eventRecord);
 
+    /**
+     * Fetches an {@code EventRecord} that is identified by an unique {@code UUID}
+     * @param uuid that uniquely identifies an {@code EventRecord}
+     * @return <p>An {@code EventRecord} identified by the {@code UUID} identifier.</p>
+     *         If an {@code EventRecord} cannot be found for the given identifier, the result is null.
+     *
+     */
 	EventRecord get(String uuid);
 
+    /**
+     * Retrieves the total count of {@code EventRecord} entities present in the underlying data store.
+     * @return An {@code Integer} count of the total number of {@code EventRecord} entities present.
+     */
 	int getTotalCount();
 
+    /**
+     * Fetches a {@code List} of {@code EventRecord} from the underlying data store that lie between an {@code Integer} range (inclusive), ordered by Identity.
+     * @param first an {@code Integer} that refers to the Identity of an {@code EventRecord}, which denotes to the first {@code EventRecord} to retrieve.
+     * @param last an {@code Integer} that refers to the Identity of an {@code EventRecord}, which denotes to the last {@code EventRecord} to retrieve.
+     * @return {@code List} of {@code EventRecord}
+     * @throws RuntimeException
+     */
 	List<EventRecord> getEventsFromRange(Integer first, Integer last);
 
+    /**
+     * Fetches a {@code List} of {@code EventRecord} from the underlying data store that lie within a {@code TimeRange}.
+     * @param timeRange a {@code TimeRange} that specifies the time range.
+     * @return {@code List} of {@code EventRecord}
+     * @throws RuntimeException
+     */
     List<EventRecord> getEventsFromTimeRange(TimeRange timeRange);
 }
