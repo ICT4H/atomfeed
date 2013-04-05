c40a360ddf3e15fc46535147df7118ea8e6c6123
vi <flawed.flint@gmail.com>
2013-04-05 20:08:53 +0530

    deleting standalone for core. added an integration test to test feed genereator bean wire up.

---
+++ atomfeed-spring-server/src/test/java/org/ict4h/atomfeed/spring/ContextTestIT.java
@@ -0,0 +1,22 @@
+package org.ict4h.atomfeed.spring;
+
+import org.ict4h.atomfeed.server.service.feedgenerator.FeedGenerator;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+@ContextConfiguration("classpath:atomFeedContext.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ContextTestIT {
+
+    @Autowired
+    private FeedGenerator feedGenerator;
+
+    @Test
+    public void shouldTestFeedGeneratorCreation(){
+        Assert.notNull(feedGenerator);
+    }
+}
