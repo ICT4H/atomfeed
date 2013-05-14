dd0c2508074061449ca25fbb99db3029c02e0ffd
Pulkit <pulkit.bosco05@gmail.com>
2013-05-14 17:12:35 +0530

    Moving InMemoryMarkerDataSource to client

---
+++ atomfeed-client/src/main/java/org/ict4h/atomfeed/client/repository/memory/InMemoryMarkerDataSource.java
@@ -0,0 +1,26 @@
+package org.ict4h.atomfeed.client.repository.memory;
+
+import org.ict4h.atomfeed.client.domain.Marker;
+import org.ict4h.atomfeed.client.repository.datasource.MarkerDataSource;
+
+import java.net.URI;
+import java.util.HashMap;
+
+public class InMemoryMarkerDataSource implements MarkerDataSource {
+
+    private final HashMap<URI,Marker> map;
+
+    public InMemoryMarkerDataSource() {
+        map = new HashMap<URI,Marker>();
+    }
+
+    @Override
+    public Marker get(URI feedUri) {
+        return map.get(feedUri);
+    }
+
+    @Override
+    public void put(Marker marker) {
+        map.put(marker.getFeedUri(),marker);
+    }
+}
