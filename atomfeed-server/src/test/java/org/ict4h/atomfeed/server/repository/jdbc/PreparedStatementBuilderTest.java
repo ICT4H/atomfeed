e8e9d3b7e365f48984bd4bc87554b1772257dfce
vi <flawed.flint@gmail.com>
2013-09-21 20:54:22 +0530

    test to verify that criteria is being set on a prepared statement

--- atomfeed-server/src/test/java/org/ict4h/atomfeed/server/repository/jdbc/PreparedStatementBuilderTest.java
+++ atomfeed-server/src/test/java/org/ict4h/atomfeed/server/repository/jdbc/PreparedStatementBuilderTest.java
@@ -3,10 +3,19 @@ package org.ict4h.atomfeed.server.repository.jdbc;
 import org.ict4h.atomfeed.server.domain.criterion.CategoryTitleCriterion;
 import org.ict4h.atomfeed.server.domain.criterion.Criterion;
 import org.ict4h.atomfeed.server.domain.criterion.EmptyCriterion;
+import org.ict4h.atomfeed.server.domain.criterion.TitleCriteria;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
 public class PreparedStatementBuilderTest {
     private static String fQTN =  "atomfeed.event_records";
@@ -28,6 +37,26 @@ public class PreparedStatementBuilderTest {
         final Criterion criterion = new CategoryTitleCriterion("category", "title");
         final String rawSql = builder.count().withCriteria(criterion).getRawSql();
         assertEquals(String.format("select count(*) from %s where category = ? and title = ?", fQTN), rawSql);
+    }
+
+    @Test
+    public void shouldBuildCountPreparedStatementWithNoCriterion() throws Exception {
+        String expectedRawSql = String.format("select count(*) from %s", fQTN);
+        Connection connection = mock(Connection.class);
+        PreparedStatement statement = mock(PreparedStatement.class);
+        when(connection.prepareStatement(expectedRawSql)).thenReturn(statement);
+        builder.count().withCriteria(new EmptyCriterion()).build(connection);
+        verify(statement,never()).setString(anyInt(),anyString());
+    }
+
+    @Test
+    public void shouldBuildCountPreparedStatementWithCriterion() throws Exception {
+        String expected = String.format("select count(*) from %s where title = ?", fQTN);
+        Connection connection = mock(Connection.class);
+        PreparedStatement statement = mock(PreparedStatement.class);
+        when(connection.prepareStatement(expected)).thenReturn(statement);
+        builder.count().withCriteria(new TitleCriteria("title")).build(connection);
+        verify(statement).setString(1,"title");
     }
 }
 
