aece178a1cd7540aab1ee1ff65036fb90227ecee
pchandra <pchandra@thoughtworks.com>
2013-12-18 14:49:54 +0530

    praveen|avoiding multiple database calls to get the total count of events for the same category

--- atomfeed-server/src/main/java/org/ict4h/atomfeed/server/service/feedgenerator/NumberFeedGenerator.java
+++ atomfeed-server/src/main/java/org/ict4h/atomfeed/server/service/feedgenerator/NumberFeedGenerator.java
@@ -1,14 +1,14 @@
 package org.ict4h.atomfeed.server.service.feedgenerator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.ict4h.atomfeed.server.domain.EventFeed;
 import org.ict4h.atomfeed.server.domain.EventRecord;
 import org.ict4h.atomfeed.server.domain.chunking.number.NumberChunkingHistory;
 import org.ict4h.atomfeed.server.domain.chunking.number.NumberRange;
 import org.ict4h.atomfeed.server.exceptions.AtomFeedRuntimeException;
 import org.ict4h.atomfeed.server.repository.AllEventRecords;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NumberFeedGenerator implements FeedGenerator {
 	private AllEventRecords allEventRecords;
@@ -21,34 +21,36 @@ public class NumberFeedGenerator implements FeedGenerator {
 
 	@Override
     public EventFeed getFeedForId(Integer feedId, String category) {
-		validateFeedId(feedId,category);
-		return findFeed(feedId,category);
+        int totalCountForCategory = allEventRecords.getTotalCountForCategory(category);
+        validateFeedId(feedId, totalCountForCategory);
+        return findFeed(feedId,category, totalCountForCategory);
 	}
 	
 	@Override
     public EventFeed getRecentFeed(String category) {
-		int latestFeed = numberChunkingHistory.getNumberOfFeeds(allEventRecords.getTotalCountForCategory(category));
+        int totalCountForCategory = allEventRecords.getTotalCountForCategory(category);
+        int latestFeed = numberChunkingHistory.getNumberOfFeeds(totalCountForCategory);
         if(isFeedZeroWithoutAnyEvents(latestFeed)){
             return new EventFeed(0,new ArrayList<EventRecord>());
         }
-		return findFeed(latestFeed,category);
+		return findFeed(latestFeed,category, totalCountForCategory);
 	}
 
     private boolean isFeedZeroWithoutAnyEvents(int latestFeed) {
         return latestFeed == 0;
     }
 
-    private EventFeed findFeed(int feedId, String category) {
-        NumberRange feedRange = numberChunkingHistory.findRange(feedId, allEventRecords.getTotalCountForCategory(category));
+    private EventFeed findFeed(int feedId, String category, int totalCountForCategory) {
+        NumberRange feedRange = numberChunkingHistory.findRange(feedId, totalCountForCategory);
 		List<EventRecord> events = allEventRecords.getEventsFromRangeForCategory(category, feedRange.getOffset(), feedRange.getLimit());
 		return new EventFeed(feedId, events);
 	}
 
-    private void validateFeedId(Integer feedId, String category) {
+    private void validateFeedId(Integer feedId, int totalCountForCategory) {
 		if ( (feedId == null) || (feedId <= 0)  ) {
 			throw new AtomFeedRuntimeException("feedId must not be null and must be greater than 0");
 		}
-		int numberOfFeeds = numberChunkingHistory.getNumberOfFeeds(allEventRecords.getTotalCountForCategory(category));
+		int numberOfFeeds = numberChunkingHistory.getNumberOfFeeds(totalCountForCategory);
 		if (feedId > numberOfFeeds) {
 			throw new AtomFeedRuntimeException("feed does not exist");
 		}
